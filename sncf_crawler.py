c47a7207d40de85efc426fd9b51c9dda8f167fef
Sparadox <etienne.lafarge@gmail.com>
2015-10-19 13:14:31 +0200

    [refactoring] Add pylintrc and refactored code

--- sncf_crawler.py
+++ sncf_crawler.py
@@ -16,26 +16,22 @@ import requests
 
 API_PREFIX = 'https://api.sncf.com/v1/coverage/sncf/'
 
-def main(path, api_key, out_json_path):
-    """ Let's crawl some data """
 
-    # Let's crawl some random data out of nowhere and save the JSON result
-    print("Crawling the {0} endpoint and saving the results into {1}".format(API_PREFIX + path,
-                                                                             out_json_path))
-    req = requests.get(API_PREFIX + path, auth=requests.auth.HTTPBasicAuth(api_key, ''))
-    with open(out_json_path, 'w') as out:
-        out.write(json.dumps(req.json(), sort_keys=True, indent=2, separators=(', ', ': ')))
-    print("Data crawled successfully")
+parser = argparse.ArgumentParser(description='Crawls data from the SNCF API')
+parser.add_argument('--path', type=str, default='',
+        help='Path to the resource (the URL part after "coverage/sncf")')
+parser.add_argument('--key', type=str, required=True,
+        help='The API key you use to connect to the SNCF API.')
+parser.add_argument('--out', type=str, default='./result.json',
+        help='Path tot he output file.')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Crawls data from the SNCF API')
-    parser.add_argument('--path', type=str, default='',
-                        help='Path to the resource (the URL part after "coverage/sncf")')
-    parser.add_argument('--key', type=str, required=True,
-                        help='The API key you use to connect to the SNCF API.')
-    parser.add_argument('--out', type=str, default='./result.json',
-                        help='Path tot he output file.')
+args = parser.parse_args()
 
-    args = parser.parse_args()
+# Let's crawl some random data out of nowhere and save the JSON result
+print("Crawling the %s endpoint and saving the results into %s" %
+      (API_PREFIX + args.path, args.out))
+req = requests.get(API_PREFIX + args.path, auth=requests.auth.HTTPBasicAuth(args.key, ''))
+with open(args.key, 'w') as out:
+    out.write(json.dumps(req.json(), sort_keys=True, indent=2, separators=(', ', ': ')))
+print("Data crawled successfully")
 
-    main(args.path, args.key, args.out)
