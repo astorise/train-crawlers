521cc11f758cf2f9745343c913f8d71087642c47
Sparadox <etienne.lafarge@gmail.com>
2015-10-19 11:11:22 +0200

    Made the crawler a bit more parametrizable

--- sncf_crawler.py
+++ sncf_crawler.py
@@ -10,24 +10,29 @@ format. This being JSON already plays rather nicely with tools like Elasticsearc
 # stdlib
 import os
 import json
+import argparse
 
 # 3p
 import requests
 
 API_PREFIX = 'https://api.sncf.com/v1/coverage/sncf/'
 
-def main():
+def main(path, api_key):
     """ Let's crawl some data """
-    api_key = os.environ.get('SNCF_API_KEY')
-    if api_key is None:
-        print('You need to provide your SNCF API key in the SNCF_API_KEY environment variable')
-        exit(0)
 
     # Let's crawl some random data out of nowhere and save the JSON result
-    req = requests.get(API_PREFIX + 'networks', auth=requests.auth.HTTPBasicAuth(api_key, ''))
+    req = requests.get(API_PREFIX + path, auth=requests.auth.HTTPBasicAuth(api_key, ''))
     with open('./results.json', 'w') as out:
         out.write(json.dumps(req.json(), sort_keys=True, indent=2, separators=(', ', ': ')))
     print("Data crawled successfully")
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Crawls data from the SNCF API')
+    parser.add_argument('--path', type=str, default='',
+                        help='Path to the resource (the URL part after "coverage/sncf")')
+    parser.add_argument('--key', type=str, required=True,
+                        help='The API key you use to connect to the SNCF API.')
+
+    args = parser.parse_args()
+
+    main(args.path, args.key)
